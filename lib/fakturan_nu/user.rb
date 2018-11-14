f9be84e2c0a0d5556f3233bca59690bad3df4f5e
Nils Öjes <nils.epost@gmail.com>
2018-11-14 15:11:25 +0100

    Add has_approved_now to User

--- lib/fakturan_nu/user.rb
+++ lib/fakturan_nu/user.rb
@@ -1,6 +1,6 @@
 module Fakturan
   class User < Base
-    attributes :login, :name
+    attributes :login, :name, :has_approved_now
 
     validates :login, presence: true
     validates :name, presence: true
